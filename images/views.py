43555119273ac07441186048688799775cd6f7a6
Julian Bürklin <jb@software-design.de>
2024-10-11 11:41:28 +0200

    api returns correct URL

--- images/views.py
+++ images/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from images.models import LottiImage
 from django.contrib.auth.decorators import user_passes_test
 from django.http import JsonResponse, HttpRequest
+from django.conf import settings
 
 def index(request):
     image = LottiImage.objects.order_by('?').first()
@@ -24,5 +25,5 @@ def bulk(request):
 
 def random(request: HttpRequest):
     image = LottiImage.objects.order_by('?').values().first()
-    image['image'] = request.build_absolute_uri(image['image'])
+    image['image'] = request.build_absolute_uri(settings.MEDIA_URL + image['image'])
     return JsonResponse(image)
